39cd82a09c63e3e3168de8060100cb48e57f4943
kotto <meshushkevich@edu.hse.ru>
2024-01-25 12:51:03 +0300

    First commit: Minimal environment was installed: Project has a following structure: -- Core ---- lib_1 ---- ... ---- lib_n -- main.cpp

---
+++ main.cpp
@@ -0,0 +1,24 @@
+//
+// Created by meshu on 1/25/2024.
+//
+
+
+/*
+   __        __
+  /\ \      / /\
+ / /\ \    / /\ \
+/ /__\ \__/_/__\ \
+\/____\ \_______\/
+        \/ /
+       / /\
+      / /\ \
+     / /__\ \
+     \_____\/
+*/
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    std::printf("");
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
